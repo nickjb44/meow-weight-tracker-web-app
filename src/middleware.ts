30837184cacbd8d6999d3e7fe420f2e6405484ef
nick buser <nicholas.buser@gmail.com>
2024-06-15 21:28:08 -0700

    fix auth protect call in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import {clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"])
 
 export default clerkMiddleware( (auth, request) => {
-    if (isProtectedRoute(request)) auth.protect
+    if (isProtectedRoute(request)) auth().protect()
     }
 );
 
