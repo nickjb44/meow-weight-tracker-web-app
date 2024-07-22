b7f05a33c996f01fb31636c23fd45fc732fea2b0
Nick Buser <nicholas.buser@gmail.com>
2024-07-22 02:17:12 -0700

    implement the router method for inserting a pet

---
+++ src/server/api/routers/insertPetPost.ts
@@ -0,0 +1,43 @@
+import {createTRPCRouter, publicProcedure} from "~/server/api/trpc";
+import {db} from "~/server/db";
+import {insertPetRequestInput} from "~/schema/insertPetRequestInput";
+import {PetPeople, Pets} from "~/server/db/schema";
+
+export const petRouter = createTRPCRouter({
+    insertPet: publicProcedure
+        .input(insertPetRequestInput)
+        .mutation(async ({input}) => {
+            const {species, gender, name, age, userId} = input;
+
+            return await db.transaction(async (trx) => {
+                const [newPet] = await trx.insert(Pets).values({
+                    Species: species,
+                    Gender: gender,
+                    Name: name,
+                    Age: age,
+                }).returning({
+                    PetID: Pets.PetID,
+                    Species: Pets.Species,
+                    Gender: Pets.Gender,
+                    Name: Pets.Name,
+                    Age: Pets.Age,
+                })
+
+                if (!newPet) {
+                    trx.rollback()
+                    throw new Error("Failed to create the pet entry")
+                }
+
+
+                await trx.insert(PetPeople).values({
+                    UserID: userId,
+                    PetID: newPet.PetID,
+                    CreatedAt: new Date(),
+                    UpdatedAt: new Date(),
+                    Role: 'Owner'
+                });
+
+                return newPet;
+            });
+        }),
+});
