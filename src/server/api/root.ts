b853fd6f783ed3e512e3fad326230018630b06b8
Nick Buser <nicholas.buser@gmail.com>
2024-07-22 02:30:06 -0700

    remove unneeded boilerplate

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { petRouter } from "~/server/api/routers/pet";
 
@@ -8,7 +7,6 @@ import { petRouter } from "~/server/api/routers/pet";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   pet: petRouter,
 });
 
